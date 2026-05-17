4d8e62bf093ee5606721c24c7f708db81ef8e304
jlroskens <jroskens@rnimbus.com>
2026-05-17 13:40:01 -0700

    remove unused kubernetes provider from required_providers

--- argocd_app/provider.tf
+++ argocd_app/provider.tf
@@ -5,9 +5,5 @@ terraform {
       source  = "argoproj-labs/argocd"
       version = ">=7.11"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">=2.38"
-    }
   }
 }
